c7fac082554203a1ed56b69c3ae6cbdcd40af08b
Amélie Barré <amliebarre@gmail.com>
2020-03-04 17:31:36 +0100

    [Feature] App architecture (#1)
    * create core module
    
    * create shared module
    
    * setup prettier

--- projects/my-pokedex/src/app/app.module.ts
+++ projects/my-pokedex/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
+
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/my-pokedex/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only'
+      );
+    }
+  }
+}
