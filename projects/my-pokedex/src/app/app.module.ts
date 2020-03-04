6d91f67d712929d4ada2b5528583cd6bba8b723a
Amélie Barré <amliebarre@gmail.com>
2020-03-04 15:05:16 +0100

    create core module

--- projects/my-pokedex/src/app/app.module.ts
+++ projects/my-pokedex/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
