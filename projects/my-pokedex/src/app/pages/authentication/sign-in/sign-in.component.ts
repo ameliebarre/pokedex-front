0288e4a046e75a973b14d755a2abb84f2420bf39
Amélie Barré <amliebarre@gmail.com>
2020-03-04 17:52:10 +0100

    create authentication module and pages

---
+++ projects/my-pokedex/src/app/pages/authentication/sign-in/sign-in.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'px-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
